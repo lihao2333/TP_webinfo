3dc793f490fc4269cec32150cd52b3253ba28e54
lihao <lihao3016@gmail.com>
2018-07-19 01:09:15 +0800

    -m

--- TP_webinfo/views.py
+++ TP_webinfo/views.py
@@ -2,4 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
 def index(request):
-    return render(request,"index.html",{})
+    As = [
+            {"name":"a1","id":1,"url":"https://www.baidu.com"},
+            {"name":"a2","id":2,"url":"https://www.baidu.com"},
+            ]
+    Bs = [
+            {"name":"b1","id":1,"url":"https://www.baidu.com"},
+            {"name":"b2","id":2,"url":"https://www.baidu.com"},
+            ]
+    Cs = [
+            {"name":"c1","id":1,"url":"https://www.baidu.com"},
+            {"name":"22","id":2,"url":"https://www.baidu.com"},
+            ]
+    Ds = [
+            {"name":"b1","id":1,"url":"https://www.baidu.com"},
+            {"name":"b2","id":2,"url":"https://www.baidu.com"},
+            ]
+    ctx={"menu":
+            [
+                {"sec":"A",
+                    "subsec":As},
+                {"sec":"B",
+                    "subsec":Bs},
+                {"sec":"C",
+                    "subsec":Cs},
+                {"sec":"D",
+                    "subsec":Ds},
+            ]
+        }
+    return render(request,"index.html",ctx)
