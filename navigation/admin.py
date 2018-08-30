d627f117699b676703cd852037c10a010f69fe41
lihao <lihao3016@gmail.com>
2018-08-30 23:15:58 +0800

    添加背景

--- navigation/admin.py
+++ navigation/admin.py
@@ -3,11 +3,11 @@ from navigation.models import Section,Subsection,Block,Main
 class SecAdmin(admin.ModelAdmin):
         list_display = ('name',)
 class SubsecAdmin(admin.ModelAdmin):
-        list_display = ('name','section', 'url')
+        list_display = ('name','section')
 class BlockAdmin(admin.ModelAdmin):
         list_display = ('image','title','content')
 class MainAdmin(admin.ModelAdmin):
-        list_display = ('title','welcome','welcome_sub')
+        list_display = ('title','welcome','welcome_sub','wallpaper','logo','tele','weibo','email','address')
 admin.site.register(Section,SecAdmin)
 admin.site.register(Subsection,SubsecAdmin)
 admin.site.register(Block,BlockAdmin)
--- navigation/models.py
+++ navigation/models.py
@@ -10,7 +10,7 @@ class Subsection(models.Model):
         return self.name
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
-    url = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="subsection",null=True ,default=None, blank=True)
     content = models.TextField()
 class Block(models.Model):
     image = models.ImageField(upload_to="block")
@@ -22,5 +22,11 @@ class Main(models.Model):
     title = models.CharField(max_length=50)
     welcome = models.CharField(max_length=50)
     welcome_sub = models.CharField(max_length=50)
+    tele = models.CharField(max_length=50 ,null=True ,default=None, blank=True)
+    weibo = models.CharField(max_length=100,null=True ,default=None, blank=True)
+    email = models.CharField(max_length=50,null=True ,default=None, blank=True)
+    address = models.CharField(max_length=51,null=True ,default=None, blank=True)
+    wallpaper = models.ImageField(upload_to="main",null=True ,default=None, blank=True)
+    logo = models.ImageField(upload_to="main",null=True ,default=None, blank=True)
     def __str__(self):
         return self.title
